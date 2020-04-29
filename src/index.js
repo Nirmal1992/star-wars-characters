63725fd8489a7c805343ea2a4719589d32285f12
Nirmal1992 <jainnirmal1992@gmail.com>
2020-04-29 20:20:35 +0530

    updated CharacterView

--- src/index.js
+++ src/index.js
@@ -1,52 +1,83 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import ReactDOM from 'react-dom';
 
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import CharacterList from './CharacterList';
-
+import CharacterView from './CharacterView';
 //import dummyData from './dummy-data';
 import endpoint from './endpoint';
 import './styles.scss';
 
-const useFetch = url => {
-  const [response, setResponse] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    setResponse([]);
-    setLoading(true);
-    setError(null);
-
-    const fetchUrl = async url => {
-      try {
-        const res = await fetch(url);
-        const data = await res.json();
-        setResponse(data);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchUrl(url + '/characters');
-    // fetch(url + '/characters')
-    //   .then(res => res.json())
-    //   .then(data => {
-    //     setResponse(data);
-    //     setLoading(false);
-    //   })
-    //   .catch(error => {
-    //     setError(error);
-    //     setLoading(false);
-    //   });
-  }, [url]);
-  return [response, loading, error];
+const initialState = {
+  response: null,
+  loading: false,
+  error: null,
 };
+
+const LOADING = 'LOADING';
+const RESPONSE_COMPLETE = 'RESPONSE_COMPLETE';
+const ERROR = 'ERROR';
+
+const reducer = (state, action) => {
+  if (action.type === LOADING) {
+    return {
+      characters: [],
+      loading: true,
+      error: null,
+    };
+  }
+
+  if (action.type === RESPONSE_COMPLETE) {
+    return {
+      characters: action.payload.data,
+      loading: false,
+      error: null,
+    };
+  }
+
+  if (action.type === ERROR) {
+    return {
+      characters: [],
+      loading: false,
+      error: action.payload.error,
+    };
+  }
+  return state;
+};
+
+const fetchCharacters = dispatch => {
+  dispatch({ type: LOADING });
+  fetch(endpoint + '/characters')
+    .then(res => res.json())
+    .then(data =>
+      dispatch({
+        type: RESPONSE_COMPLETE,
+        payload: { data: data.characters },
+      }),
+    )
+    .catch(error => {
+      dispatch({
+        type: ERROR,
+        payload: { error },
+      });
+    });
+};
+const useThunkReducer = (reducer, initialState) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const enhancedDispatched = action => {
+    if (typeof action === 'function') {
+      action(dispatch);
+    }
+    dispatch(action);
+  };
+  return [state, enhancedDispatched];
+};
+
 const Application = () => {
-  const [response, loading, error] = useFetch(endpoint);
-  const characters = (response && response.characters) || [];
+  const [state, dispatch] = useThunkReducer(reducer, initialState);
+  const { characters, loading, error } = state;
 
   if (error) {
     return (
@@ -62,11 +93,18 @@ const Application = () => {
       </header>
       <main>
         <section className="sidebar">
+          <button onClick={() => dispatch(fetchCharacters)}>
+            {' '}
+            Fetch characters
+          </button>
           {loading ? (
             <div> Loading...</div>
           ) : (
             <CharacterList characters={characters} />
           )}
+        </section>
+        <section className="CharacterView">
+          <Route path="/characters/:id" component={CharacterView} />
         </section>
       </main>
     </div>
